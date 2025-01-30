8b020baaf4e4ef1516952e2c1ff1b850cb4106f3
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-30 14:20:29 +0200

    Enhance Header component with improved navigation styling and responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,23 +5,38 @@ import { selectIsLoggedIn } from "../../redux/auth/selectors";
 const Header = () => {
 	const isLoggedIn = useSelector(selectIsLoggedIn);
 	return (
-		<header className="font-display w-auto flex justify-between p-2 bg-amber-100 shadow-md mx-4 my-4 rounded-lg">
+		<header className="font-display w-auto flex justify-between p-2 bg-amber-100 shadow-md mx-4 my-4 rounded-lg items-center flex-wrap">
 			<Link to="/" className="px-3 py-2 font-bold text-xl">
 				Contacts Hub
 			</Link>
 			<nav>
-				<ul className="flex">
+				<ul className="flex gap-8 flex-wrap">
 					<li>
-						<NavLink to="/">Contacts</NavLink>
+						<NavLink
+							to="/"
+							className="py-2 px-3 transform transition-all duration-300 ease-in-out hover:scale-110 inline-block"
+						>
+							Contacts
+						</NavLink>
 					</li>
 					{!isLoggedIn && (
 						<li>
-							<NavLink to="/register">Register</NavLink>
+							<NavLink
+								to="/register"
+								className="py-2 px-3 transform transition-all duration-300 ease-in-out hover:scale-110 inline-block"
+							>
+								Register
+							</NavLink>
 						</li>
 					)}
 					{!isLoggedIn && (
 						<li>
-							<NavLink to="/login">Log In</NavLink>
+							<NavLink
+								to="/login"
+								className="py-2 px-3 transform transition-all duration-300 ease-in-out hover:scale-110 inline-block"
+							>
+								Log In
+							</NavLink>
 						</li>
 					)}
 				</ul>
