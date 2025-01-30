67a9c45355b3e176ce9f85093490166a3b843241
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-30 13:39:34 +0200

    Add conditional rendering for authentication links in Layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { NavLink, Outlet } from "react-router";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Layout = () => {
+	const isLoggedIn = useSelector(selectIsLoggedIn);
+
 	return (
 		<>
 			<nav>
@@ -8,12 +12,16 @@ const Layout = () => {
 					<li>
 						<NavLink to="/">Contacts</NavLink>
 					</li>
-					<li>
-						<NavLink to="/register">Register</NavLink>
-					</li>
-					<li>
-						<NavLink to="/login">Log In</NavLink>
-					</li>
+					{!isLoggedIn && (
+						<li>
+							<NavLink to="/register">Register</NavLink>
+						</li>
+					)}
+					{!isLoggedIn && (
+						<li>
+							<NavLink to="/login">Log In</NavLink>
+						</li>
+					)}
 				</ul>
 			</nav>
 			<Outlet />
