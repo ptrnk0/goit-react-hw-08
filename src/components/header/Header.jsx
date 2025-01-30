f1e5830ec11e654511086d6a3ce3aea7a398f70a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-30 14:03:04 +0200

    Refactor Header component for improved styling and navigation links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,12 @@ import { selectIsLoggedIn } from "../../redux/auth/selectors";
 const Header = () => {
 	const isLoggedIn = useSelector(selectIsLoggedIn);
 	return (
-		<header>
-			<Link>Contacts Hub</Link>
+		<header className="font-display w-auto flex justify-between p-2 bg-amber-100 shadow-md mx-4 my-4 rounded-lg">
+			<Link to="/" className="px-3 py-2 font-bold text-xl">
+				Contacts Hub
+			</Link>
 			<nav>
-				<ul>
+				<ul className="flex">
 					<li>
 						<NavLink to="/">Contacts</NavLink>
 					</li>
