2e1a158114d0dbb7a2f4ac175cc1e1b1d8a11a5d
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-30 13:51:17 +0200

    Update title to 'Contacts Hub' and refactor Header component structure

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,30 @@
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Header = () => {
 	const isLoggedIn = useSelector(selectIsLoggedIn);
 	return (
-		<nav>
-			<ul>
-				<li>
-					<NavLink to="/">Contacts</NavLink>
-				</li>
-				{!isLoggedIn && (
+		<header>
+			<Link>Contacts Hub</Link>
+			<nav>
+				<ul>
 					<li>
-						<NavLink to="/register">Register</NavLink>
+						<NavLink to="/">Contacts</NavLink>
 					</li>
-				)}
-				{!isLoggedIn && (
-					<li>
-						<NavLink to="/login">Log In</NavLink>
-					</li>
-				)}
-			</ul>
-		</nav>
+					{!isLoggedIn && (
+						<li>
+							<NavLink to="/register">Register</NavLink>
+						</li>
+					)}
+					{!isLoggedIn && (
+						<li>
+							<NavLink to="/login">Log In</NavLink>
+						</li>
+					)}
+				</ul>
+			</nav>
+		</header>
 	);
 };
 
