6f0ee5dfc22584475b11097106960ce7f41a7f2a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-02 19:34:13 +0200

    Enhance HomePage component with improved styling and layout adjustments

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -9,9 +9,9 @@ const Home = () => {
 					"url(https://img.freepik.com/free-vector/gradient-black-background-with-wavy-lines_23-2149158560.jpg?t=st=1738516237~exp=1738519837~hmac=45eb5890c4d4a35eaf92e3c7b54f28d5018c8cfb9a4d9ed0a89031c28df5a237&w=1800)",
 			}}
 		>
-			<div className="hero-overlay"></div>
-			<div className="hero-content text-neutral-content text-center">
-				<div className="max-w-lg">
+			<div className="hero-overlay "></div>
+			<div className="hero-content text-neutral-content text-center  backdrop-blur-xs rounded-xl p-10">
+				<div className="max-w-lg ">
 					<h1 className="mb-5 text-5xl font-bold">Hello there</h1>
 					<p className="mb-5">
 						Easily create, organize, and store important contacts in
