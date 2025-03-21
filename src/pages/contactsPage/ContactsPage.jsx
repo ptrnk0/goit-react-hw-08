2bb51f5b9ef440fe4386524759751f5b31b3d1ef
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 17:34:37 +0200

    Refactor ContactsPage component for improved layout and responsiveness

--- src/pages/contactsPage/ContactsPage.jsx
+++ src/pages/contactsPage/ContactsPage.jsx
@@ -8,27 +8,27 @@ import { selectError, selectLoading } from "../../redux/contacts/selectors";
 import Loader from "../../components/loader/Loader";
 
 const Contacts = () => {
-	const dispatch = useDispatch();
-	const isLoading = useSelector(selectLoading);
-	const error = useSelector(selectError);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
-	useEffect(() => {
-		dispatch(fetchContacts());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-	return (
-		<div className="flex gap-12">
-			<ContactForm />
-			<div className="grow-1">
-				<SearchBox />
-				<div className="flex items-center flex-col relative">
-					{error && <p>Something went wrong.</p>}
-					{isLoading && <Loader />}
-					<ContactList />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex gap-12 flex-col md:flex-row">
+      <ContactForm />
+      <div className="grow-1">
+        <SearchBox />
+        <div className="flex items-center flex-col relative">
+          {error && <p>Something went wrong.</p>}
+          {isLoading && <Loader />}
+          <ContactList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
