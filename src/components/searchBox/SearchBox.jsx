befaeb3a0674a9826103bbc54ac819be94da5842
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 17:34:31 +0200

    Refactor SearchBox component for improved layout and styling consistency

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -3,29 +3,27 @@ import { selectFilter } from "../../redux/filter/selectors";
 import { changeFilter } from "../../redux/filter/slice";
 
 const SearchBox = () => {
-	const filter = useSelector(selectFilter);
-	const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
+  const dispatch = useDispatch();
 
-	const handleInput = (value) => {
-		dispatch(changeFilter(value));
-	};
+  const handleInput = (value) => {
+    dispatch(changeFilter(value));
+  };
 
-	return (
-		<div>
-			<fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-				<legend className="fieldset-legend">
-					Search contacts by name
-				</legend>
-				<input
-					type="text"
-					placeholder="Find contacts by name"
-					value={filter}
-					onInput={(evt) => handleInput(evt.target.value.trim())}
-					className="input"
-				/>
-			</fieldset>
-		</div>
-	);
+  return (
+    <div>
+      <fieldset className="fieldset w-full md:w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+        <legend className="fieldset-legend">Search contacts by name</legend>
+        <input
+          type="text"
+          placeholder="Find contacts by name"
+          value={filter}
+          onInput={(evt) => handleInput(evt.target.value.trim())}
+          className="input w-full"
+        />
+      </fieldset>
+    </div>
+  );
 };
 
 export default SearchBox;
