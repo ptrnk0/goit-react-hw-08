e145b7d6e914539450ba15906b8de27093a913cf
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 17:33:46 +0200

    Refactor Contact component to improve button styling and enhance user interaction

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,22 +2,26 @@ import { useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contacts/operations";
 
 const Contact = ({ contact, line }) => {
-	const dispatch = useDispatch();
-	const handleDelete = () => {
-		dispatch(deleteContact(contact.id));
-	};
-	return (
-		<>
-			<th>{line}</th>
-			<td>{contact.name}</td>
-			<td>{contact.number}</td>
-			<td>
-				<button type="button" onClick={handleDelete}>
-					Delete
-				</button>
-			</td>
-		</>
-	);
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+  return (
+    <>
+      <th>{line}</th>
+      <td>{contact.name}</td>
+      <td>{contact.number}</td>
+      <td>
+        <button
+          type="button"
+          onClick={handleDelete}
+          className="border-2 p-1 rounded-sm cursor-pointer hover:scale-110 transition-all ease-in"
+        >
+          Delete
+        </button>
+      </td>
+    </>
+  );
 };
 
 export default Contact;
