a3de89335024335fbe1a61011f513a24fb20dd8b
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 17:34:25 +0200

    Refactor Layout component for improved margin styling and layout consistency

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router";
 import AppBar from "../appBar/AppBar";
 
 const Layout = () => {
-	return (
-		<div className="flex flex-col h-screen">
-			<AppBar />
-			<main className="font-display px-10 mt-10 grow">
-				<Outlet />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex flex-col h-screen">
+      <AppBar />
+      <main className="font-display m-10 grow">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
