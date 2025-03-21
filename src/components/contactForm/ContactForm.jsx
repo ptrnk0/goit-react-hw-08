81731d3e5f1c131df81327f52460f5c0880265c8
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 17:34:03 +0200

    Refactor ContactForm component for improved layout and styling consistency

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -4,68 +4,68 @@ import { addContact } from "../../redux/contacts/operations";
 import { useDispatch } from "react-redux";
 
 const contactFormSchema = Yup.object().shape({
-	name: Yup.string()
-		.min(3, "Name must be at least 3 characters")
-		.max(50, "Name must be at most 50 characters")
-		.required("Name is required"),
-	number: Yup.string()
-		.min(3, "Number must be at least 3 characters")
-		.max(50, "Number must be at most 50 characters")
-		.required("Number is required"),
+  name: Yup.string()
+    .min(3, "Name must be at least 3 characters")
+    .max(50, "Name must be at most 50 characters")
+    .required("Name is required"),
+  number: Yup.string()
+    .min(3, "Number must be at least 3 characters")
+    .max(50, "Number must be at most 50 characters")
+    .required("Number is required"),
 });
 
 const ContactForm = () => {
-	const dispatch = useDispatch();
-	const initialValues = { name: "", number: "" };
+  const dispatch = useDispatch();
+  const initialValues = { name: "", number: "" };
 
-	const handleSubmit = (values, actions) => {
-		dispatch(addContact(values));
-		actions.resetForm(initialValues);
-	};
+  const handleSubmit = (values, actions) => {
+    dispatch(addContact(values));
+    actions.resetForm(initialValues);
+  };
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			validationSchema={contactFormSchema}
-			onSubmit={handleSubmit}
-		>
-			<Form>
-				<fieldset className="fieldset w-50 bg-base-200 shadow-lg p-4 rounded-box">
-					<legend end className="fieldset-legend">
-						Add contact
-					</legend>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={contactFormSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <fieldset className="fieldset w-full md:w-50 bg-base-200 shadow-lg p-4 rounded-box">
+          <legend end className="fieldset-legend">
+            Add contact
+          </legend>
 
-					<label className="fieldset-label" htmlFor="name">
-						Name
-					</label>
-					<Field
-						id="name"
-						type="text"
-						name="name"
-						placeholder="Name"
-						className="input"
-					/>
-					<ErrorMessage name="name" component="span"></ErrorMessage>
+          <label className="fieldset-label" htmlFor="name">
+            Name
+          </label>
+          <Field
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="input w-full"
+          />
+          <ErrorMessage name="name" component="span"></ErrorMessage>
 
-					<label className="fieldset-label" htmlFor="number">
-						Phone number
-					</label>
-					<Field
-						id="number"
-						type="text"
-						name="number"
-						placeholder="Phone number"
-						className="input"
-					/>
-					<ErrorMessage name="number" component="span"></ErrorMessage>
+          <label className="fieldset-label" htmlFor="number">
+            Phone number
+          </label>
+          <Field
+            id="number"
+            type="text"
+            name="number"
+            placeholder="Phone number"
+            className="input w-full"
+          />
+          <ErrorMessage name="number" component="span"></ErrorMessage>
 
-					<button className="btn btn-neutral mt-4" type="submit">
-						Add
-					</button>
-				</fieldset>
-			</Form>
-		</Formik>
-	);
+          <button className="btn btn-neutral mt-4" type="submit">
+            Add
+          </button>
+        </fieldset>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
